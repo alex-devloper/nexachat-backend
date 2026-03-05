c1071d42b94e6106ce44a37d92165d327e61c58a
Chirajuddin Ahmed <chirajul_20@example.com>
2026-03-05 15:24:10 +0530

    fix socket cors for production

--- src/server.ts
+++ src/server.ts
@@ -12,12 +12,14 @@ import userRoutes from "./routes/user"; // ✅✅✅ ADDED
 import Message from "./models/Message";
 import Conversation from "./models/Conversation";
 import User from "./models/User";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Types } = mongoose;
 
 // ================= DB =================
 mongoose
-  .connect("mongodb://127.0.0.1:27017/nexachat")
+  .connect(process.env.MONGO_URI as string)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log("Mongo Error ❌", err));
 
@@ -217,7 +219,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   },
 });
@@ -541,4 +543,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000 🚀"));
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
